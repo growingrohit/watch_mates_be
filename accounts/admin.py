83cceb4358069b048cf682acdee0c58b17dabf89
growing-rohit <rkc30.09.2000@gmail.com>
2026-05-02 18:20:34 +0530

    feat | added the admin for both models

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,89 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as DjangoUserAdmin
 
-# Register your models here.
+from accounts.models import Profile, User
+
+
+@admin.register(User)
+class UserAdmin(DjangoUserAdmin):
+    list_display = (
+        "username",
+        "full_name",
+        "mobile_number",
+        "is_active",
+        "is_staff",
+    )
+    search_fields = ("username", "email", "full_name", "mobile_number")
+    list_filter = ("is_active", "is_staff", "is_superuser")
+    readonly_fields = ("created_at", "updated_at", "full_name")
+    save_as = True
+    fieldsets = (
+        (None, {"fields": ("username", "password")}),
+        (
+            "Personal info",
+            {
+                "fields": (
+                    "first_name",
+                    "last_name",
+                    "full_name",
+                    "email",
+                    "country_code",
+                    "mobile_number",
+                )
+            },
+        ),
+        (
+            "Permissions",
+            {
+                "fields": (
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                    "groups",
+                    "user_permissions",
+                )
+            },
+        ),
+        (
+            "Important dates",
+            {
+                "fields": (
+                    "last_login",
+                    "date_joined",
+                    "created_at",
+                    "updated_at",
+                )
+            },
+        ),
+    )
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": (
+                    "username",
+                    "email",
+                    "first_name",
+                    "last_name",
+                    "country_code",
+                    "mobile_number",
+                    "password1",
+                    "password2",
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                ),
+            },
+        ),
+    )
+
+
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ("user", "display_name", "date_of_birth", "is_active")
+    search_fields = ("user__username", "display_name")
+    list_filter = ("is_active",)
+    list_select_related = ("user",)
+    readonly_fields = ("created_at", "updated_at")
+    save_as = True
