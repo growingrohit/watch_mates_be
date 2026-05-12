7add6848e7b4a3839384c504c188cc23e68ea461
growing-rohit <rkc30.09.2000@gmail.com>
2026-05-12 18:46:37 +0530

    feat | added last message and kind in thread

--- chat/models/thread.py
+++ chat/models/thread.py
@@ -2,13 +2,30 @@ from django.db import models
 
 from common.abstract import AbstractCompleteAudit
 
+
+class ThreadKind(models.TextChoices):
+    DIRECT = "direct", "Direct"
+    GROUP = "group", "Group"
+    CHANNEL = "channel", "Channel"
+    PRIVATE = "private", "Private"
+
+
 class Thread(AbstractCompleteAudit):
     name = models.CharField(
         max_length=255, blank=True
     )
     profile_image = models.URLField(blank=True)
-    is_group = models.BooleanField(
-        default=False
+    kind = models.CharField(
+        max_length=255,
+        choices=ThreadKind.choices,
+        default=ThreadKind.DIRECT,
+    )
+    last_message = models.ForeignKey(
+        "chat.ThreadMessage",
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+        related_name="thread_last_message",
     )
 
     class Meta:
@@ -19,3 +36,11 @@ class Thread(AbstractCompleteAudit):
 
     def __str__(self):
         return f"{self.name} - {self.created_by}"
+
+    
+    def update_last_message(self):
+        self.last_message = self.threadmessage_set.filter(
+            is_active=True
+        ).order_by("-created_at")
+
+        self.save(fields=["last_message"])
