df1b7624b1a9dfe637dfcbb7f06984d4f54306de
growing-rohit <rkc30.09.2000@gmail.com>
2026-05-12 19:33:40 +0530

    feat | Add complete audit for message

--- chat/models/threadmessage.py
+++ chat/models/threadmessage.py
@@ -1,20 +1,17 @@
 from django.db import models
 
-from common.abstract import AbstractAudit
+from common.abstract import AbstractCompleteAudit
 
 from chat.models import Thread
 from accounts.models import Profile
 
-class ThreadMessage(AbstractAudit):
+class ThreadMessage(AbstractCompleteAudit):
     thread = models.ForeignKey(
         Thread,
         on_delete=models.CASCADE,
     )
     message = models.TextField()
-    created_by = models.ForeignKey(
-        Profile,
-        on_delete=models.CASCADE,
-    )
+    
     
     def __str__(self):
         return f"{self.thread} - {self.created_by}"
