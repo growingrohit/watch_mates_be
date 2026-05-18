1f7374f65b1821e2dc4c5609f64c9fb262a6d0dd
growing-rohit <rkc30.09.2000@gmail.com>
2026-05-18 18:56:17 +0530

    test | login tests

---
+++ accounts/tests/test_login.py
@@ -0,0 +1,75 @@
+from django.contrib.auth import get_user_model
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase
+
+User = get_user_model()
+
+
+class LoginAPITestCase(APITestCase):
+    url = reverse("login")
+
+    @classmethod
+    def setUpTestData(cls):
+        cls.password = "securepass123"
+        cls.user = User.objects.create_user(
+            username="loginuser",
+            email="loginuser@example.com",
+            password=cls.password,
+            first_name="Login",
+            last_name="User",
+        )
+
+    def test_login_success(self):
+        response = self.client.post(
+            self.url,
+            {"username": "loginuser", "password": self.password},
+            format="json",
+        )
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data["message"], "Login successful.")
+        self.assertEqual(response.data["data"]["username"], "loginuser")
+        self.assertEqual(response.data["data"]["email"], "loginuser@example.com")
+        self.assertEqual(response.data["data"]["full_name"], "Login User")
+        self.assertEqual(str(self.user.id), response.data["data"]["id"])
+
+    def test_login_user_does_not_exist(self):
+        response = self.client.post(
+            self.url,
+            {"username": "unknown_user", "password": self.password},
+            format="json",
+        )
+
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+        self.assertEqual(response.data["message"], "User does not exist.")
+
+    def test_login_invalid_password(self):
+        response = self.client.post(
+            self.url,
+            {"username": "loginuser", "password": "wrongpassword"},
+            format="json",
+        )
+
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+        self.assertEqual(response.data["message"], "Password is invalid.")
+
+    def test_login_missing_username(self):
+        response = self.client.post(
+            self.url,
+            {"password": self.password},
+            format="json",
+        )
+
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertIn("username", response.data)
+
+    def test_login_missing_password(self):
+        response = self.client.post(
+            self.url,
+            {"username": "loginuser"},
+            format="json",
+        )
+
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertIn("password", response.data)
