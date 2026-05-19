ff5b0a563c072c805832a71df4dfd299d19f7f3b
growing-rohit <rkc30.09.2000@gmail.com>
2026-05-19 20:50:26 +0530

    feat | api for refresh token

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
+from rest_framework_simplejwt.views import TokenRefreshView
 
 from accounts.views import LoginAPIView, UserProfileCreateAPIView
 
 urlpatterns = [
     path("users/", UserProfileCreateAPIView.as_view(), name="user-profile-create"),
     path("login/", LoginAPIView.as_view(), name="login"),
+    path("token/refresh/", TokenRefreshView.as_view(), name="token-refresh"),
 ]
